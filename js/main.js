291db2dbdb600945ebf808cd4a3b9fd1ea8c2240
sychayev <47305110+sychayev@users.noreply.github.com>
2024-08-28 00:55:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(function () {
+const menuBurger = document.querySelector('.header__menu-burger');
+const comMenu = document.querySelector('.header__menu');
 
+menuBurger.addEventListener('click', () => {
+	menuBurger.classList.toggle('active');
+	comMenu.classList.toggle('active');
 })
